c1d5f7e6fc2f4c76140b5dfae8dbb283202c0d28
sunnieho0323 <s224863464@deakin.edu.au>
2025-08-10 11:49:23 +1000

    created controllers and routes

---
+++ routes/petRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const activityController = require('../controllers/petController');
+
+// add your pet routes here
+
+
+
+// endpoints for pet routes
+
+module.exports = router;
