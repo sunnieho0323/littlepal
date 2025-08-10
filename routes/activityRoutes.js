db38cc4148db83c7cbf197b71c320feceb56be1d
sunnieho0323 <s224863464@deakin.edu.au>
2025-08-10 12:06:33 +1000

    add controller details

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -3,7 +3,11 @@ const activityController = require('../controllers/activityController');
 
 // add your activity routes here
 
+// GET /api/activities?petId=...
+router.get('/', activityController.listByPet);
 
+// POST /api/activities
+router.post('/', activityController.add);
 
 // endpoints for activity routes
 
