94802f275520ade280a731efa8be79aae24b5e9a
sunnieho0323 <s224863464@deakin.edu.au>
2025-08-30 14:10:38 +1000

    week7 parc cloud

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.use(cors());
-app.use(express.json());
+app.use(express.static('public'));
+
+// io
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+  setInterval(() => {
+    socket.emit('number', parseInt(Math.random() * 10));
+  }, 1000);
+});
 
 // routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/pets', require('./routes/petRoutes'));
 app.use('/api/activities', require('./routes/activityRoutes'));
 
-app.get('/', (req, res) => res.send('Little Pal API is running'));
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+//app.get('/', (req, res) => res.send('Little Pal API is running'));
+
+http.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
